22b361750a3052b66c64536042a6e667ce8a2d4f
yanni4night <yanni4night@gmail.com>
2014-09-17 21:06:40 +0800

    clean roamssi.js

--- roamssi.js
+++ roamssi.js
@@ -9,31 +9,38 @@ var fs = require('fs'),
 var exec = require('child_process').exec;
 
 
-var sys={
-    cssurl : './static/release/main.css',
-    jsurl : './static/release/pcroam_main.js'
+var sys = {
+    cssurl: './static/release/main.css',
+    jsurl: './static/release/pcroam_main.js'
 };
-    
 
-var filefolder = path.join(__dirname , '.');
 
-    
-var css = fs.readFileSync(path.join(filefolder , sys.cssurl)).toString();
+var filefolder = path.join(__dirname, '.');
+
+
+var css = fs.readFileSync(path.join(filefolder, sys.cssurl)).toString();
 var js = fs.readFileSync(path.join(filefolder, sys.jsurl)).toString();
-js = uglify.minify(js , {fromString:true}).code;
-css= new cleancss({keepSpecialComments:0,noAdvanced:1}).minify(css);
+js = uglify.minify(js, {
+    fromString: true
+}).code;
+css = new cleancss({
+    keepSpecialComments: 0,
+    noAdvanced: 1
+}).minify(css);
 
 
-if(!fs.existsSync(path.join(__dirname,'landing'))){
-    fs.mkdirSync(path.join(__dirname,'landing'));
+if (!fs.existsSync(path.join(__dirname, 'landing'))) {
+    fs.mkdirSync(path.join(__dirname, 'landing'));
 }
 
-fs.writeFileSync(path.join(__dirname,'landing' , 'roam_common.js') , js);
-fs.writeFileSync(path.join(__dirname,'landing' , 'common.css') , css);
+fs.writeFileSync(path.join(__dirname, 'landing', 'roam_common.js'), js);
+fs.writeFileSync(path.join(__dirname, 'landing', 'common.css'), css);
 
 
 
-exec("rsync -avz landing root@10.12.143.85:/search/wan/webapp/static/",function(){
+exec("rsync -avz landing root@10.12.143.85:/search/wan/webapp/static/", function() {
     exec('rm -rf landing');
 });
-//exec("sshpass -p 'noSafeNoWork@2014' scp -r landing root@10.11.201.212:/search/wan/webapp/static;sshpass -p 'noSafeNoWork@2014' ssh root@10.11.201.212 'sh /search/script/publishscript/rsync_static2m1_new.sh'",function(){exec('rm -rf landing')});
+/*exec("sshpass -p 'noSafeNoWork@2014' scp -r landing root@10.11.201.212:/search/wan/webapp/static;sshpass -p 'noSafeNoWork@2014' ssh root@10.11.201.212 'sh /search/script/publishscript/rsync_static2m1_new.sh'", function() {
+    exec('rm -rf landing')
+});*/
